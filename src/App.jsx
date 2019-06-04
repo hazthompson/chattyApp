c989575a560fdc43322e210806bd876825449434
Harriet Thompson <hazthomps@yahoo.co.uk>
2019-06-04 18:43:38 +0000

    initial components set up and app constructor created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state= {messages:[]};
+    this.state= {currentUser:''};
+  }
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+      <nav className="navbar">
+        <a href="/" className="navbar-brand">Chatty</a>
+      </nav>
+      <MessageList />
+      <ChatBar />
+      </div>
     );
   }
 }
