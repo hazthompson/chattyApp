225ef38d00336fac886d5bc42d04391152ffd19d
Harriet Thompson <hazthomps@yahoo.co.uk>
2019-06-04 21:03:46 +0000

    connected the Enter press in ChatBar to App function

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,11 +1,27 @@
 import React, {Component} from 'react';
 
 class ChatBar extends Component {
+  constructor(props){
+    super(props);
+     
+    // this.state = {
+    //   content: '',
+    //   error: '',
+    //   visable: {
+    //     compose:true
+    //   }
+    // }
+
+    // this.onCompose = this.onCompose.bind(this);
+    // this.onContent = this.onContent.bind(this);
+    // this.onPost = this.onPost.bind(this);
+  }
   render() {
+    const POST_LENGTH_MAX = 140;
     return (
       <footer className="chatbar">
         <input className="chatbar-username" defaultValue={this.props.currentUser.name}/>
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input className="chatbar-message" onKeyDown={this.props.onEnter} placeholder="Type a message and hit ENTER" />
       </footer>
     );
   }
