f7fb00be81e8ae74fa687cd3d19540d7c61fec8e
Harriet Thompson <hazthomps@yahoo.co.uk>
2019-06-05 18:10:24 -0700

    message received from server and printed to webpage as new message with correct username

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,15 +1,17 @@
-
-const express = require('express');
-const SocketServer = require('ws').Server;
+const express = require("express");
+const SocketServer = require("ws").Server;
+const uuidv1 = require("uuid/v1");
 
 // Set the port to 3001
 const PORT = 3001;
 
 // Create a new express server
 const server = express()
-   // Make the express server serve static assets (html, javascript, css) from the /public folder
-  .use(express.static('public'))
-  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+  // Make the express server serve static assets (html, javascript, css) from the /public folder
+  .use(express.static("public"))
+  .listen(PORT, "0.0.0.0", "localhost", () =>
+    console.log(`Listening on ${PORT}`)
+  );
 
 // Create the WebSockets server
 const wss = new SocketServer({ server });
@@ -17,14 +19,34 @@ const wss = new SocketServer({ server });
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
-wss.on('connection', (ws) => {
-  console.log('Client connected');
- 
-wss.on('message', (message) => {
-  console.log("Got a new message: ", message);
-  wss.broadcast(message);
-});  
+wss.on("connection", ws => {
+  console.log("Client connected");
+
+  ws.on("message", message => {
+    const post = JSON.parse(message).data;
+
+    post.id = uuidv1();
+    console.log(post);
+    console.log(
+      `Got a new message:
+      ${post.content}
+      from user:
+      ${post.username}`
+    );
+
+    const postSend = JSON.stringify(post);
+
+    ws.broadcast = function broadcast(data) {
+      console.log("reached broadcast func");
+      wss.clients.forEach(function each(client) {
+        // if (client.readyState === wss.OPEN) {
+        client.send(data);
+        console.log("data sent: ", data);
+      });
+    };
+    ws.broadcast(postSend);
+  });
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  ws.on('close', () => console.log('Client disconnected'));
+  ws.on("close", () => console.log("Client disconnected"));
 });
